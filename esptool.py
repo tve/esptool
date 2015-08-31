84e268776e20acc89cba9f7e9872ee907ab11621
Angus Gratton <gus@projectgus.com>
2015-08-31 10:26:08 +1000

    Use byte strings for protocol communications data
    (Equivalent to strings on Python 2, but byte strings on Python 3.)

--- esptool.py
+++ esptool.py
@@ -77,10 +77,10 @@ class ESPROM:
     ESP_OTP_MAC1    = 0x3ff00054
 
     # Sflash stub: an assembly routine to read from spi flash and send to host
-    SFLASH_STUB     = "\x80\x3c\x00\x40\x1c\x4b\x00\x40\x21\x11\x00\x40\x00\x80" \
-            "\xfe\x3f\xc1\xfb\xff\xd1\xf8\xff\x2d\x0d\x31\xfd\xff\x41\xf7\xff\x4a" \
-            "\xdd\x51\xf9\xff\xc0\x05\x00\x21\xf9\xff\x31\xf3\xff\x41\xf5\xff\xc0" \
-            "\x04\x00\x0b\xcc\x56\xec\xfd\x06\xff\xff\x00\x00"
+    SFLASH_STUB     = b"\x80\x3c\x00\x40\x1c\x4b\x00\x40\x21\x11\x00\x40\x00\x80" \
+            b"\xfe\x3f\xc1\xfb\xff\xd1\xf8\xff\x2d\x0d\x31\xfd\xff\x41\xf7\xff\x4a" \
+            b"\xdd\x51\xf9\xff\xc0\x05\x00\x21\xf9\xff\x31\xf3\xff\x41\xf5\xff\xc0" \
+            b"\x04\x00\x0b\xcc\x56\xec\xfd\x06\xff\xff\x00\x00"
 
     def __init__(self, port = 0, baud = ESP_ROM_BAUD):
         self._port = serial.Serial(port)
@@ -92,15 +92,15 @@ class ESPROM:
 
     """ Read bytes from the serial port while performing SLIP unescaping """
     def read(self, length = 1):
-        b = ''
+        b = b''
         while len(b) < length:
             c = self._port.read(1)
-            if c == '\xdb':
+            if c == b'\xdb':
                 c = self._port.read(1)
-                if c == '\xdc':
-                    b = b + '\xc0'
-                elif c == '\xdd':
-                    b = b + '\xdb'
+                if c == b'\xdc':
+                    b = b + b'\xc0'
+                elif c == b'\xdd':
+                    b = b + b'\xdb'
                 else:
                     raise CommunicationError('Invalid SLIP escape')
             else:
@@ -109,7 +109,7 @@ class ESPROM:
 
     """ Write bytes to the serial port while performing SLIP escaping """
     def write(self, packet):
-        buf = '\xc0'+(packet.replace('\xdb','\xdb\xdd').replace('\xc0','\xdb\xdc'))+'\xc0'
+        buf = b'\xc0'+(packet.replace(b'\xdb',b'\xdb\xdd').replace(b'\xc0',b'\xdb\xdc'))+b'\xc0'
         self._port.write(buf)
 
     """ Calculate checksum of a blob, as it is defined by the ROM """
@@ -147,7 +147,7 @@ class ESPROM:
 
     """ Perform a connection test """
     def sync(self):
-        self.command(ESPROM.ESP_SYNC, '\x07\x07\x12\x20'+32*'\x55')
+        self.command(ESPROM.ESP_SYNC, b'\x07\x07\x12\x20'+32*b'\x55')
         for i in range(7):
             self.command()
 
@@ -182,69 +182,69 @@ class ESPROM:
     """ Read memory address in target """
     def read_reg(self, addr):
         res = self.command(ESPROM.ESP_READ_REG, struct.pack('<I', addr))
-        if res[1] != "\0\0":
+        if res[1] != b"\0\0":
             raise CommunicationError('Failed to read target memory')
         return res[0]
 
     """ Write to memory address in target """
     def write_reg(self, addr, value, mask, delay_us = 0):
         if self.command(ESPROM.ESP_WRITE_REG,
-                struct.pack('<IIII', addr, value, mask, delay_us))[1] != "\0\0":
+                struct.pack('<IIII', addr, value, mask, delay_us))[1] != b"\0\0":
             raise CommunicationError('Failed to write target memory')
 
     """ Start downloading an application image to RAM """
     def mem_begin(self, size, blocks, blocksize, offset):
         if self.command(ESPROM.ESP_MEM_BEGIN,
-                struct.pack('<IIII', size, blocks, blocksize, offset))[1] != "\0\0":
+                struct.pack('<IIII', size, blocks, blocksize, offset))[1] != b"\0\0":
             raise CommunicationError('Failed to enter RAM download mode')
 
     """ Send a block of an image to RAM """
     def mem_block(self, data, seq):
         if self.command(ESPROM.ESP_MEM_DATA,
-                struct.pack('<IIII', len(data), seq, 0, 0)+data, ESPROM.checksum(data))[1] != "\0\0":
+                struct.pack('<IIII', len(data), seq, 0, 0)+data, ESPROM.checksum(data))[1] != b"\0\0":
             raise CommunicationError('Failed to write to target RAM')
 
     """ Leave download mode and run the application """
     def mem_finish(self, entrypoint = 0):
         if self.command(ESPROM.ESP_MEM_END,
-                struct.pack('<II', int(entrypoint == 0), entrypoint))[1] != "\0\0":
+                struct.pack('<II', int(entrypoint == 0), entrypoint))[1] != b"\0\0":
             raise CommunicationError('Failed to leave RAM download mode')
 
     """ Start downloading to Flash (performs an erase) """
     def flash_begin(self, size, offset):
         old_tmo = self._port.timeout
-        num_blocks = (size + ESPROM.ESP_FLASH_BLOCK - 1) / ESPROM.ESP_FLASH_BLOCK
+        num_blocks = (size + ESPROM.ESP_FLASH_BLOCK - 1) // ESPROM.ESP_FLASH_BLOCK
 
         sectors_per_block = 16
         sector_size = 4096
-        num_sectors = (size + sector_size - 1) / sector_size
-        start_sector = offset / sector_size
+        num_sectors = (size + sector_size - 1) // sector_size
+        start_sector = offset // sector_size
 
         head_sectors = sectors_per_block - (start_sector % sectors_per_block)
         if num_sectors < head_sectors:
             head_sectors = num_sectors
 
         if num_sectors < 2 * head_sectors:
-            erase_size = (num_sectors + 1) / 2 * sector_size
+            erase_size = (num_sectors + 1) // 2 * sector_size
         else:
             erase_size = (num_sectors - head_sectors) * sector_size
 
         self._port.timeout = 10
         if self.command(ESPROM.ESP_FLASH_BEGIN,
-                struct.pack('<IIII', erase_size, num_blocks, ESPROM.ESP_FLASH_BLOCK, offset))[1] != "\0\0":
+                struct.pack('<IIII', erase_size, num_blocks, ESPROM.ESP_FLASH_BLOCK, offset))[1] != b"\0\0":
             raise CommunicationError('Failed to enter Flash download mode')
         self._port.timeout = old_tmo
 
     """ Write block to flash """
     def flash_block(self, data, seq):
         if self.command(ESPROM.ESP_FLASH_DATA,
-                struct.pack('<IIII', len(data), seq, 0, 0)+data, ESPROM.checksum(data))[1] != "\0\0":
+                struct.pack('<IIII', len(data), seq, 0, 0)+data, ESPROM.checksum(data))[1] != b"\0\0":
             raise CommunicationError('Failed to write to target Flash')
 
     """ Leave flash mode and run/reboot """
     def flash_finish(self, reboot = False):
         pkt = struct.pack('<I', int(not reboot))
-        if self.command(ESPROM.ESP_FLASH_END, pkt)[1] != "\0\0":
+        if self.command(ESPROM.ESP_FLASH_END, pkt)[1] != b"\0\0":
             raise CommunicationError('Failed to leave Flash mode')
 
     """ Run application code in flash """
@@ -290,12 +290,12 @@ class ESPROM:
         # Fetch the data
         data = ''
         for _ in xrange(count):
-            if self._port.read(1) != '\xc0':
+            if self._port.read(1) != b'\xc0':
                 raise CommunicationError('Invalid head of packet (sflash read)')
 
             data += self.read(size)
 
-            if self._port.read(1) != '\xc0':
+            if self._port.read(1) != b'\xc0':
                 raise CommunicationError('Invalid end of packet (sflash read)')
 
         return data
@@ -600,14 +600,14 @@ if __name__ == '__main__':
             esp.flash_begin(blocks*esp.ESP_FLASH_BLOCK, address)
             seq = 0
             while len(image) > 0:
-                print('\rWriting at 0x%08x... (%d %%)' % (address + seq*esp.ESP_FLASH_BLOCK, 100*(seq+1)/blocks))
+                print('Writing at 0x%08x... (%d %%)' % (address + seq*esp.ESP_FLASH_BLOCK, 100*(seq+1)/blocks), end='\r')
                 sys.stdout.flush()
                 block = image[0:esp.ESP_FLASH_BLOCK]
                 # Fix sflash config data
-                if address == 0 and seq == 0 and block[0] == '\xe9':
+                if address == 0 and seq == 0 and block[0] == b'\xe9':
                     block = block[0:2] + flash_info + block[4:]
                 # Pad the last block
-                block = block + '\xff' * (esp.ESP_FLASH_BLOCK-len(block))
+                block = block + b'\xff' * (esp.ESP_FLASH_BLOCK-len(block))
                 esp.flash_block(block, seq)
                 image = image[esp.ESP_FLASH_BLOCK:]
                 seq += 1
